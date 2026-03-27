13a917ab57eb405e8d639e8ce09786dd5470c0c4
manelHowlin1 <mhowlin@mail.austral.edu.ar>
2026-03-27 10:23:29 -0300

    Update exercise_weekday.py

--- exercise_weekday.py
+++ exercise_weekday.py
@@ -17,4 +17,8 @@ def weekday():
         Para la entrada "domingo", la salida esperada es:
         Fin de semana
     """
-    pass
+        dia = input().lower()
+    if not dia == "sabado" and not dia == "domingo":
+        print("Dia habil")
+    else:
+        print("Fin de semana")
